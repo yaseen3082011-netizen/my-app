672ac12f25ae30929455e32dcac32082a1a8dfaf
yaseen3082011-netizen <yaseen3082011@gmail.com>
2026-08-17 00:14:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("welcome to youtube channel name generator?")
+user_name = input ("what is your nikname?\n")
+content = input ("what is your channel about?\n")
+print("you can name your channel ("+ content + " with " + user_name + ")")
